dd5ae428bb55f282aa1b6e262e93d485a39f4fee
HugoLattard <hugolattard@gmail.com>
2021-02-06 16:57:37 +0100

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require("../assets/data");
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(items),
     // body: JSON.stringify(person), // Body MUST RETURN A STRING
