48c96993442f7bd83fc3346ed5b278d0962b6959
HugoLattard <hugolattard@gmail.com>
2021-02-06 17:01:23 +0100

    hello cors error fixed

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -4,6 +4,9 @@
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: "Our First Netlify Function Example OK",
     // body: JSON.stringify(person), // Body MUST RETURN A STRING
